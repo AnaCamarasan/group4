d5c1892d3ec21d3c6567d4180b7eeb7fde6e5a24
EM-R_M <emily.manton@aol.com>
2024-10-28 09:52:04 +0000

    First part of constructors complete

---
+++ GoingFutherWithClasses/src/two/constructors/Car.java
@@ -0,0 +1,51 @@
+package two.constructors;
+
+public class Car {
+
+    private String model;
+    private String brand;
+    private int year;
+
+    public Car(String model, String brand, int year) {
+        this.model = model;
+        this.brand = brand;
+        this.year = year;
+    }
+
+    public Car(String model, String brand) {
+        this(model, brand, 2024);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", brand='" + brand + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
---
+++ GoingFutherWithClasses/src/two/constructors/Main.java
@@ -0,0 +1,12 @@
+package two.constructors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car1 = new Car("Mini", "BMW", 2019);
+        System.out.println(car1);
+        Car car2 = new Car("Golf", "Volkswagen");
+        System.out.println(car2);
+    }
+
+}
