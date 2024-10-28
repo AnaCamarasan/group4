202b370330437ee9696fe97e9d9c7f50d3ece0c3
EM-R_M <emily.manton@aol.com>
2024-10-28 16:20:20 +0000

    First half of task completed

---
+++ AbstractInheritance/src/Main.java
@@ -0,0 +1,19 @@
+import inheritance.Animal;
+import inheritance.Cat;
+import inheritance.Dog;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Dog animal1 = new Dog("Tony", 4, "hot-dog", "small");
+        Cat animal2 = new Cat("Maddy", 3, "Tabby", false);
+
+        Animal[] animalList = {animal1, animal2};
+
+        for (Animal animal : animalList){
+            animal.noise();
+        }
+
+    }
+}
---
+++ AbstractInheritance/src/inheritance/Dog.java
@@ -0,0 +1,25 @@
+package inheritance;
+
+public class Dog extends Animal {
+    public String breed;
+    public String size;
+
+    public Dog(String name, int noOfLegs, String breed, String size) {
+        super(name, noOfLegs);
+        this.breed = breed;
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "breed='" + breed + '\'' +
+                ", size='" + size + '\'' +
+                "} " + super.toString();
+    }
+
+    @Override
+    public void noise() {
+        System.out.println("woof woof");
+    }
+}
