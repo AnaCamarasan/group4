11ae500ac63ceb32fc1b28088949dd038d7e6d49
AnaCamarasan <ana.camarasan@gmail.com>
2024-10-29 14:03:08 +0000

    created order class

--- InventoryManagement/src/inventory/management/Order.java
+++ InventoryManagement/src/inventory/management/Order.java
@@ -1,4 +1,55 @@
 package inventory.management;
 
 public class Order {
+    private final int orderId;
+    private int productId;
+    private int customerId;
+
+    private int quantity;
+    private static int nextOrderId = 0;
+
+    public Order(int productId, int customerId, int quantity) {
+        this.orderId = nextOrderId++;
+        this.productId = productId;
+        this.customerId = customerId;
+        this.quantity = quantity;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId=" + orderId +
+                ", productId=" + productId +
+                ", customerId=" + customerId +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
